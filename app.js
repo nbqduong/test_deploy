d1297a3d9b536cb523dfea2167240c9581c0032d
nbqduong <86090755+nbqduong@users.noreply.github.com>
2025-10-07 09:52:32 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! My simple app is now live on DigitalOcean.');
+  res.send('Hello, World! My simple app is now live on asdfasd Duong asdfadsf DigitalOcean.');
 });
 
 app.listen(port, () => {
